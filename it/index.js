27cca6746e847560fead8e5637641aa7b3bca418
NEO2708 <parthr.2608@gmail.com>
2022-07-31 20:57:56 +0530

    sdc

---
+++ it/index.js
@@ -0,0 +1,38 @@
+function myFunction() {
+    var checkBox = document.getElementById("eng");
+    var text = document.getElementById("engch");
+    if (checkBox.checked == true) {
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+}
+
+function gujshow() {
+    var checkBox = document.getElementById("guj");
+    var text = document.getElementById("gujch");
+    if (checkBox.checked == true) {
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+}
+
+function uguj() {
+    var checkBox = document.getElementById("guj");
+    var text = document.getElementById("gujch");
+    if (checkBox.checked == true) {
+        checkBox.checked = false;
+        text.style.display = "none";
+    }
+
+}
+function ueng() {
+    var checkBox = document.getElementById("eng");
+    var text = document.getElementById("engch");
+    if (checkBox.checked == true) {
+        checkBox.checked = false;
+        text.style.display = "none";
+    }
+
+}
